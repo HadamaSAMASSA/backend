e0e71fd502b7af421f21dcf05680a2cdb3863cf3
unknown <samassaadama@hotmail.fr>
2021-03-01 17:25:44 +0100

    chapitre1 exo1

--- chapitre_01/index.js
+++ chapitre_01/index.js
@@ -2,10 +2,42 @@ const express = require('express');
 const app = express();
 
 const port = 8000;
-app.listen(port, () => {
-  console.log('Server started on port: ' + port);
-});
+
+const authorsId = [];
+const notAuthorsId = [];
 
 app.get('/', (req, res) => {
     res.send('Authors API');
   });
+
+for(let i = 0; i < 10; i ++) {
+    authorsId + i;
+}
+if(authorsId == 1){
+    app.get(`/authors/${authorsId}`, (req, res) => {
+    res.send('Lawrence Nowell, UK');
+  })
+}else if(authorsId == 2){
+    app.get(`/authors/${authorsId}`, (req, res) => {
+        res.send('William Shakespeare, UK');
+      })
+}else if(authorsId == 3){
+    app.get(`/authors/${authorsId}`, (req, res) => {
+        res.send('Charles Dickens, US');
+      })
+}else if(authorsId == 4){
+    app.get(`/authors/${authorsId}`, (req, res) => {
+        res.send('Oscar Wilde, UK');
+      })
+}/*else{
+    app.get("*", (req, res) => {
+        res.send(`The author with the ID ${notAuthorsId} does not exist`);
+}*/
+
+
+
+  app.listen(port, () => {
+  console.log('Server started on port: ' + port);
+});
+
+// JE SAIS QUE C4EST FAUX MAIS BON
